2c5483b97cad23530c91bbed8f643912809ac85c
Richard6141 <somasse-richard.salanon@epitech.eu>
2026-01-18 00:52:39 +0100

    Add citadelle.salanon.info to CORS whitelist

--- server/index.ts
+++ server/index.ts
@@ -20,6 +20,8 @@ const allowedOrigins = [
   'http://localhost:8080',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:8080',
+  'https://citadelle.salanon.info',
+  'http://citadelle.salanon.info',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
